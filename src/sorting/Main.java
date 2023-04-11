aad3f6bee5acf6e3a3b23a089175992b1adcb44b
Fasukuwa Kamanda <kamanda000@gmail.com>
2023-04-11 19:43:00 -0400

    Command line argurments are parsed and anaylzed based on the data type

--- src/sorting/Main.java
+++ src/sorting/Main.java
@@ -3,20 +3,88 @@ package sorting;
 import java.util.*;
 
 public class Main {
-    public static void main(final String[] args) {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        if(args[0].equals("-dataType")){
+            String type = args[1];
+            switch (type) {
+                case "long" -> {
+                    ArrayList<Integer> numList = new ArrayList<>();
+                    int number;
+                    while (scanner.hasNextInt()) {
+                        number = scanner.nextInt();
+                        numList.add(number);
+                    }
+                    int maxValue = Collections.max(numList);
+                    int numFrequency = Collections.frequency(numList, maxValue);
+                    double percentage =(double)numFrequency/numList.size()*100;
+                    System.out.println("Total numbers: " + numList.size() + ".");//Number of elements in the list(numbers)
+                    System.out.printf("The greatest number: %d (%d times(s), %.0f%%."
+                            , maxValue, numFrequency, percentage);//maximum value in the list (number)
+                }
+                case "line" -> {
+                    ArrayList<String> lineList = new ArrayList<>();
+                    while(scanner.hasNextLine()){
+                        lineList.add(scanner.nextLine().trim());
+                    }
+                    String longestLine = lineList
+                            .stream()
+                            .max(Comparator.comparingInt(String::length))
+                            .get();
+                    int totalLines = lineList.size();
+                    int lineFrequency = Collections.frequency(lineList, longestLine);
+                    double percentage = (double)lineFrequency/totalLines*100;
+                    System.out.printf("Total lines: %d.\nThe longest line:\n%s\n(%d times(s), %.0f%%)."
+                            ,totalLines, longestLine, lineFrequency, percentage);
+                }
+                case "word" -> {
+                    ArrayList<String> wordList = new ArrayList<>();
+                    String word;
+                    while(scanner.hasNext()){
+                        word = scanner.next();
+                        wordList.add(word);
+                    }
+                    //System.out.println(wordList);
+                    int totalWords = wordList.size();
+                    String longestWord = wordList
+                            .stream()
+                            .max(Comparator.comparingInt(String::length))
+                            .get();
 
-        long max; //max value of all numbers
-        int maxCount; //frequency of the max value
-        ArrayList<Long> list = new ArrayList<>();
-        while (scanner.hasNextLong()) {
-            long number = scanner.nextLong();
-            list.add(number);
+                    int wordFrequency = Collections.frequency(wordList, longestWord);
+                    double percentage = (double)wordFrequency/totalWords*100;
+                    System.out.printf("Total words: %d.\nThe longest word: %s (%d time(s), %.0f%%)."
+                    , totalWords, longestWord, wordFrequency, percentage);
+                }
+                default -> System.out.println("Incorrect input");
+            }
+        }else{
+            //Assumed to be of word type
+            ArrayList<String> wordList = new ArrayList<>();
+            String word;
+            while(scanner.hasNext()){
+                word = scanner.next();
+                wordList.add(word);
+            }
+            int totalWords = wordList.size();
+            String longestWord = Collections.max(wordList);
+            int wordFrequency = Collections.frequency(wordList, longestWord);
+            double percentage = (double)wordFrequency/totalWords*100;
+            System.out.printf("Total words: %d.\nThe longest word: %s (%d time(s), %f%%)."
+                    , totalWords, longestWord, wordFrequency, percentage);
         }
-        Collections.sort(list);
-        max = list.get(list.size()-1);
-        maxCount = Collections.frequency(list, max);
-        System.out.println("Total numbers: "+list.size()+".");
-        System.out.println("The greatest number: "+ max+ " (" +maxCount+" time(s).");
+
     }
+
+    /*public static void word(){
+
+    }
+
+    public static void longer(){
+
+    }
+
+    public static void line(){
+
+    }*/
 }
